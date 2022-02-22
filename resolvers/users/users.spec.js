181c1ab7485fcab06a08155fe99d7e1562a7dd8d
Alao-Abiodun <46402042+Alao-Abiodun@users.noreply.github.com>
2022-02-22 13:24:55 +0100

    intial commit

---
+++ resolvers/users/users.spec.js
@@ -0,0 +1,63 @@
+const User = require("../../models/user");
+const resolvers = require("../user");
+const { setupDB } = require("../test-setup");
+
+setupDB("users");
+
+describe("getUser", () => {
+  it("Should save user to database", async (done) => {
+    const newUser = new User({
+      name: "alao",
+      email: "abiodu@gmail.com",
+      username: "abbey",
+      password: "plop",
+      mobile_number: "0091838384",
+      country: "Ghana",
+    });
+    await newUser.save();
+
+    const fetchedUser = await resolvers.Query.getUser(
+      {},
+      { id: newUser.id },
+      { userId: newUser.id }
+    );
+
+    expect(fetchedUser.id).toBe(newUser.id);
+
+    done();
+  });
+  it("Should throw if not authenticated", async (done) => {
+    const newUser = new User({
+      name: "alao",
+      email: "abiodu@gmail.com",
+      username: "abbey",
+      password: "plop",
+      mobile_number: "0091838384",
+      country: "Ghana",
+    });
+    await newUser.save();
+
+    await expect(
+      resolvers.Query.getUser({}, { id: newUser.id }, {})
+    ).rejects.toThrow(/must be authenticated/);
+
+    done();
+  });
+  it("Should throw if not right user", async (done) => {
+    const newUser = new User({
+      name: "alao",
+      email: "abiodu@gmail.com",
+      username: "abbey",
+      password: "plop",
+      mobile_number: "0091838384",
+      country: "Ghana",
+    });
+    await newUser.save();
+
+    await expect(
+      resolvers.Query.getUser({}, { id: newUser.id }, { userId: "123" })
+    ).rejects.toThrow(/own datas/);
+
+    done();
+  });
+});
